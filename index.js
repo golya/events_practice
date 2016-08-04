4deccec9d14fb2899ee03f560c86585b60e69d55
Ádám Gólya <adam.stork@gmail.com>
2016-08-04 12:49:04 +0200

    feat(index.js): initial add of an example emitter

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const util = require('util');
+const EventEmitter = require('events');
+
+function TestEmitter() {
+  EventEmitter.call(this);
+}
+
+util.inherits(TestEmitter, EventEmitter);
+
+const test = new TestEmitter();
+
+test.on('push', function() {
+  console.log('push');
+});
+
+test.emit('push');
