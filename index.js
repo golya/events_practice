622f1abc72265de9b1f5e7a90c98928c05f5b0fb
Ádám Gólya <adam.stork@gmail.com>
2016-08-04 13:09:34 +0200

    feat(index.js): add asynchron event

--- index.js
+++ index.js
@@ -8,10 +8,18 @@ function TestEmitter() {
   EventEmitter.call(this);
 }
 
+function AsyncMethod() {
+  return Promise.resolve('done');
+}
+
 const test = new TestEmitter();
 
 test.on('push', function(msg) {
   console.log('push', msg);
+  AsyncMethod().then((result) => {
+    console.log(result);
+  });
+  console.log('event end');
 });
 
 test.emit('push', 'my message');
